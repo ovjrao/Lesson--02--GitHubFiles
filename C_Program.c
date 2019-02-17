8cfc13797f6932733a605f7a7ab43fbe2e57dafc
root <ovjrao@hotmail.com>
2019-02-17 22:19:52 +0000

    Pushing the files once again for refresh

--- C_Program.c
+++ C_Program.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-printf(“Hello! I am C-Program. Thank you!”);
+printf(“Hello! I am C-Program. Thank you again!”);
 return 0;
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var texts = “I am your JavaScript Program”;
+var texts = “I am your JavaScript Program again”;
 console.log(texts);
 
